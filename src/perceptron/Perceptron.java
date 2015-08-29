2c3d0cbad0a21694e3e9ab46d11e0b9bc869f2a4
Ashwin <balathinnappan@gmail.com>
2015-08-29 10:51:05 +0530

    Comments added.

--- src/perceptron/Perceptron.java
+++ src/perceptron/Perceptron.java
@@ -18,16 +18,10 @@ public class Perceptron {
         {1,1} ,
         {1,0} ,
         {0,1} ,
-        {0,0} } ; 
+        {0,0} } ;
     static int[] target = { 1, -1, -1, -1 } ;
-    static float bias ;
-    static float out ;
-    static float w1, w2, b_new ;
+    static float out, threshold, alpha, y_in, output, w1, w2, b_new, bias ;
     static boolean stopping ;
-    static float threshold ;
-    static float alpha ;
-    static float y_in ;
-    static float output  ;
     static int epoch ;
     static FileWriter writer ;
     static File file ;
@@ -43,12 +37,21 @@ public class Perceptron {
         Perceptron.alpha = alpha ;
         epoch = 0 ;
     }
+    /**
+     * 
+     * @return 
+     * @throws IOException
+    */
     public static int execute()throws IOException{
+        /*
+        * Runs each epoch.
+        * While loop breaks after all the targets same as the net values.
+        */
         while( !stopping && epoch <= 20 ) {
            stopping = true ;
            epoch++ ;
             for( int i=0 ;i<4; i++){
-                y_in = b_new + ( ( inputs[i][0]*w1 )+( inputs[i][1]*w2 ) ) ;
+                y_in = b_new + ( ( inputs[i][0]*w1 )+( inputs[i][1]*w2 ) ) ; //net = b(new)+((x1*w1)+(x2*w2))
                 if(y_in > threshold )
                     output = 1 ;
                 else if( y_in < -threshold )
@@ -56,9 +59,9 @@ public class Perceptron {
                 else
                     output = 0 ;
                 if( output != target[i] ){
-                    w1 = ( w1+( alpha*target[i]*inputs[i][0] ) ) ;
+                    w1 = ( w1+( alpha*target[i]*inputs[i][0] ) ) ; // w(new) = w(old)+(alpha*target*x)
                     w2 = ( w2+( alpha*target[i]*inputs[i][1] ) ) ;
-                    b_new = (int)( b_new + ( alpha*target[i] ) ) ;
+                    b_new = (int)( b_new + ( alpha*target[i] ) ) ; //b(new) = (old)+(alpha*x)
                     stopping = false ;
                 }
                 System.out.println( inputs[i][0]+"\t"+inputs[i][1]+"\t"+bias+"\t"+(int)y_in+"\t"+output+"\t"+target[i]+"\t"+
@@ -75,7 +78,7 @@ public class Perceptron {
      * @throws IOException
      */
     public static void main(String[] args)throws IOException {
-        file = new File( "/home/ashwin/Desktop/float_bias.csv" ) ;
+        file = new File( "float_bias.csv" ) ;
         writer = new FileWriter( file ) ;
         writer.append( "threshold/learning_rate," ) ;
         for( int i=10 ;i<=100; i++){
@@ -89,9 +92,9 @@ public class Perceptron {
             for( int j=10 ;j<=100; j++){
                 alpha = j/10f ;
                 System.out.println( threshold ) ;
-                new Perceptron(threshold, alpha); 
+                new Perceptron(threshold, alpha); //Creates a new Perceptron.
                 epoch = Perceptron.execute() ;
-                writer.append(((epoch<20)?epoch:Double.POSITIVE_INFINITY)+",");
+                writer.append(((epoch<20)?epoch:Double.POSITIVE_INFINITY)+","); //Writes the output into output.csv file.
             }
             writer.append("\n") ;
         }
